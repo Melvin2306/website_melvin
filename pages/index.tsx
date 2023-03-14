5411b4a4c672ec73da8f33d5027744fd36d7c8e3
Melvin2306 <melvin.rinkleff@gmx.de>
2023-03-14 12:19:23 +0100

    added name, deleted pictures and changed favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +13,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        Test
+        This is the website of Melvin Rinkleff
+        <br />
+        Melvin Rinkleff
+        <br />
+        Thomasstraße 75
+        <br />
+        12053 Berlin
       </div>
     </>
   )
